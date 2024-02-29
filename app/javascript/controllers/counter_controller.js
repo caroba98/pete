296bbcbaa9864c136e67ce86d68205cc22c987e4
Caroline Baumeister <caroline@baumeister-dv.de>
2024-02-29 11:16:49 +0100

    counter works

--- app/javascript/controllers/counter_controller.js
+++ app/javascript/controllers/counter_controller.js
@@ -2,24 +2,49 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="counter"
 export default class extends Controller {
-  static targets = ["value"]
+  static targets = ["countries", "meters", "tours"]
 
   connect() {
     console.log("hi");
-    this.startCounter();
+    // this.startCounter();
+    this.startLoopOne();
+    this.startLoopTwo();
+    this.startLoopThree();
   }
 
-  startCounter() {
-    const speed = 200;
-    const value = this.valueTarget.getAttribute('akhi');
-    const data = this.valueTarget.innerText;
-    const time = value / speed;
-    if(data < value) {
-      this.valueTarget.innerText = Math.ceil(data + time);
-        setTimeout(this.startCounter, 1);
-      // }else{
-      //   this.valueTarget.innerText = value;
+  startLoopOne() {
+    let count = 0;
+    const number = this.countriesTarget.getAttribute('country');
+    const interval = setInterval(() => {
+      this.countriesTarget.innerText = count;
+      count++;
+      if (count > number) {
+        clearInterval(interval);
       }
+    }, 20); // Loop every second
   }
 
+  startLoopTwo() {
+    let count = 0;
+    const number = this.metersTarget.getAttribute('height');
+    const interval = setInterval(() => {
+      this.metersTarget.innerText = count;
+      count += 10;
+      if (count > number) {
+        clearInterval(interval);
+      }
+    }, 2); // Loop every second
+  }
+
+  startLoopThree() {
+    let count = 0;
+    const number = this.toursTarget.getAttribute('alps');
+    const interval = setInterval(() => {
+      this.toursTarget.innerText = count;
+      count++;
+      if (count > number) {
+        clearInterval(interval);
+      }
+    }, 50); // Loop every second
+  }
 }
